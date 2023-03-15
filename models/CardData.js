05aa5bda1144336bb542936cd6f58731e5e5ebe8
roshni <roshni798200@gmail.com>
2023-03-15 00:38:19 -0700

    Initial commits

---
+++ models/CardData.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const CardSchema = new mongoose.Schema({
+    name: String,
+    mobile: Number,
+    email: String,
+    dob: String,
+    totalLimit: Number,
+    avLimit: Number,
+    cardNumber: String,
+    holderName: String,
+    exDate: String,
+    cvv: Number
+})
+
+const CardModal = mongoose.model('CardModel', CardSchema);
+
+module.exports = CardModal;
---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+const CardModal = require('../models/CardData')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/form', function(req, res, next) {
+  res.render('form', { title: 'Express' });
+});
+router.post('/card', function(req, res, next) {
+  CardModal({
+    name: req.body.name,
+    mobile: req.body.mobile,
+    email: req.body.email,
+    dob: req.body.dob,
+    totalLimit: req.body.totalLimit,
+    avLimit: req.body.avLimit,
+    cardNumber: req.body.cardNumber,
+    holderName: req.body.holderName,
+    exDate: req.body.expiry,
+    cvv: req.body.cvv
+  }).save()
+  res.render('success')
+});
+
+module.exports = router;
